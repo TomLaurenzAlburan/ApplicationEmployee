cbd55d4dc16d4c06ab1da41cff361e9936011da3
briannnnn18 <brianjamesceniza2788@gmail.com>
2024-10-20 22:17:00 +0800

    Add project files.

---
+++ ApplicationEmployee/IEmployee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeInterface
+{
+    public interface IEmployee
+    {
+        string Name { get; set; }
+        string LastName { get; set; }
+        string JobTitle { get; set; }
+        double RatePerHour { get; set; }
+        double HoursWorked { get; set; }
+        double CalculateSalary();
+    }
+}
---
+++ ApplicationEmployee/PartTimeEmployee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EmployeeInterface;
+
+namespace EmployeeNamespace
+{
+    public class PartTimeEmployee : IEmployee
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string JobTitle { get; set; }
+        public double RatePerHour { get; set; }
+        public double HoursWorked { get; set; }
+
+        public double CalculateSalary()
+        {
+            return RatePerHour * HoursWorked;
+        }
+    }
+}
